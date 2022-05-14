08cfaaee41c8bbcdca7a620ee9122223d769e027
Julien Bernard <artessbm@gmail.com>
2022-05-14 22:11:02 +0200

    Fix binary issues

--- include/global.hpp
+++ include/global.hpp
@@ -2,15 +2,74 @@
 #define __GLOBAL_HPP
 
 #include "pico/stdlib.h"
-#include <stdio.h>
-
-const uint8_t gccDataPins[4] = {13, 12, 11, 10};
-
-const uint8_t oscilloPins[3] = {2, 3, 4};
 
 const int us = 125;
 
 #define LED_PIN 25
-#define USB_POWER_PIN 24
+
+
+#pragma region logs
+
+#define USE_UART0 0
+#define USE_UART1 0
+
+inline void initialize_uart() {
+    #if USE_UART0
+    // Initialise UART 0
+    uart_init(uart0, 115200);
+ 
+    // Set the GPIO pin mux to the UART - 0 is TX, 1 is RX
+    gpio_set_function(0, GPIO_FUNC_UART);
+    gpio_set_function(1, GPIO_FUNC_UART);
+    #endif
+    #if USE_UART1
+    // Initialise UART 0
+    uart_init(uart1, 115200);
+ 
+    // Set the GPIO pin mux to the UART - 0 is TX, 1 is RX
+    gpio_set_function(8, GPIO_FUNC_UART);
+    gpio_set_function(9, GPIO_FUNC_UART);
+    #endif
+
+}
+inline void log_uart0(const char* str) {
+    #if USE_UART0
+    uart_puts(uart0, str);
+    #endif
+    #if USE_UART1
+    uart_puts(uart1, str);
+    #endif
+}
+inline void log_uart0_int(int i) {
+    #if USE_UART0
+    char str[16];
+    sprintf(str, "%d", i);
+    uart_puts(uart0, str);
+    #endif
+    #if USE_UART1
+    char str[16];
+    sprintf(str, "%d", i);
+    uart_puts(uart1, str);
+    #endif
+}
+
+inline void log_uart0_array(uint8_t *ptr, uint16_t len) {
+    #if USE_UART0
+    log_uart0("array len=");log_uart0_int(len);log_uart0("\n");
+    for (int i = 0; i<len; i++) {
+        log_uart0_int(ptr[i]); log_uart0(" ");
+    }
+    log_uart0("\n");
+    #endif
+    #if USE_UART1
+    log_uart0("array len=");log_uart0_int(len);log_uart0("\n");
+    for (int i = 0; i<len; i++) {
+        log_uart0_int(ptr[i]); log_uart0(" ");
+    }
+    log_uart0("\n");
+    #endif
+}
+
+#pragma endregion
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include "global.hpp"
 
 #include "usb/logic.hpp"
-#include "rgb_led.hpp"
 
 #include "hardware/structs/systick.h"
 
@@ -13,6 +12,8 @@ int main() {
     // Clock at 125MHz
     set_sys_clock_khz(us*1000, true);
     
+    initialize_uart();
+
     gpio_init(25);
     gpio_set_dir(25, GPIO_OUT);
 
